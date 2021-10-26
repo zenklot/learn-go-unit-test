eb8f4009ebc671dcf1d7e651ae034d8ec86b3ffd
zenklot <zenklot11@gmail.com>
2021-10-26 09:33:14 +0700

    writing with failed test

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,11 +1,42 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestHelloWorld(t *testing.T) {
 	result := HelloWorld("Goz")
 
 	if result != "Hello Goz" {
-		panic("Result Is Not Hello Goz")
+		t.Fail()
 	}
+	fmt.Println("Test Hello World 1 Done")
+}
+
+func TestHelloWorldDua(t *testing.T) {
+	result := HelloWorld("Dua")
+
+	if result != "Hello Dua" {
+		t.FailNow()
+	}
+	fmt.Println("Test Hello World 2 Done")
+}
+
+func TestHelloWorldTiga(t *testing.T) {
+	result := HelloWorld("Goz")
+
+	if result != "Hello Goz" {
+		t.Error("harusnya Hello Goz")
+	}
+	fmt.Println("Test Hello World 3 Done")
+}
+
+func TestHelloWorldEmpat(t *testing.T) {
+	result := HelloWorld("Goz")
+
+	if result != "Hello Goz" {
+		t.Fatal("Harusnya hello Goz")
+	}
+	fmt.Println("Test Hello World 4 Done")
 }
